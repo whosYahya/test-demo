d5d0a25a44ec121e3c14e5f6347b287e6747a72e
whosYahya <ikidsmag@gmail.com>
2026-03-10 16:27:33 +0530

    fix: remove amc-test submodule

--- playwright.config.js
+++ playwright.config.js
@@ -36,6 +36,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: process.env.BASE_URL || 'http://localhost:8004',
     storageState: path.join(__dirname, 'utils', 'authState.json'),
+    extraHTTPHeaders: {
+      'ngrok-skip-browser-warning': '1',
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
